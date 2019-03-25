ffc0961cf1bf6840fd3303af0b9a0f7ca4c0ac50
steven martin lister <steven.martin-lister@gsi.io>
2019-03-25 20:40:06 +0000

    controller and domain added

---
+++ game/src/main/java/com/fortyquestions/controllers/CrudController.java
@@ -0,0 +1,7 @@
+package com.fortyquestions.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CrudController {
+}
---
+++ game/src/main/java/com/fortyquestions/domain/question.java
@@ -0,0 +1,12 @@
+package com.fortyquestions.domain;
+
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+public class question {
+
+}
